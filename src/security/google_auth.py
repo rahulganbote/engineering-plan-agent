162f591600f516a79559452229c10aaa3deea968
rahulganbote <122250836+rahulganbote@users.noreply.github.com>
2026-06-11 20:26:53 -0500

    fix: add rel=opener to auth button to enable window.opener popups redirection

--- src/security/google_auth.py
+++ src/security/google_auth.py
@@ -149,7 +149,7 @@ def _get_auth_button_html(target: str) -> str:
         font-weight: 500 !important;
     }}
     </style>
-    <a href="{url}" target="{target}" class="g-signin-btn">
+    <a href="{url}" target="{target}" rel="opener" class="g-signin-btn">
         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" viewBox="0 0 48 48" style="margin-right: 10px; vertical-align: middle;">
           <g>
             <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
